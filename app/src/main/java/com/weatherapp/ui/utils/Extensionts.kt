5d9b27e17bdd6967a4180e601ae98504cc76e3ef
mirshafiee <b.mirshafiee90@gmail.com>
2022-12-09 20:52:54 +0100

    adding images for weather conditions

--- app/src/main/java/com/weatherapp/ui/utils/Extensionts.kt
+++ app/src/main/java/com/weatherapp/ui/utils/Extensionts.kt
@@ -5,6 +5,6 @@ import com.weatherapp.R
 fun chooseDrawableAccordingToWeatherState(weatherCode : Int) : Int{
 
     return when(weatherCode){
-        else -> R.drawable.ic_question_mark_default
+        else -> R.drawable.ic_default_weather
     }
 }
