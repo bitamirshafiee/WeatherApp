da5521b248b10481280a50ea4ff3f99215b6b2f3
mirshafiee <b.mirshafiee90@gmail.com>
2022-12-09 15:14:54 +0100

    add weather details screen

---
+++ app/src/main/java/com/weatherapp/ui/weatherdetails/WeatherDetails.kt
@@ -0,0 +1,12 @@
+package com.weatherapp.ui.weatherdetails
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WeatherDetails() {
+    Column(modifier = Modifier.fillMaxSize()) {
+    }
+}
