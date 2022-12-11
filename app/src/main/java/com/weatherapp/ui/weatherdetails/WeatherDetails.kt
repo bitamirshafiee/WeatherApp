aa8d8f59d97527cea8bf1550e822d8a95a9aee5a
mirshafiee <b.mirshafiee90@gmail.com>
2022-12-11 16:55:11 +0100

    get current location

--- app/src/main/java/com/weatherapp/ui/weatherdetails/WeatherDetails.kt
+++ app/src/main/java/com/weatherapp/ui/weatherdetails/WeatherDetails.kt
@@ -1,17 +1,33 @@
 package com.weatherapp.ui.weatherdetails
 
 import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.core.app.ActivityCompat
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
 import com.weatherapp.ui.utils.Permission
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun WeatherDetails(weatherDetailsViewModel: WeatherDetailsViewModel) {
+    val context = LocalContext.current
+    val fusedLocationClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(context)
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -20,6 +36,20 @@ fun WeatherDetails(weatherDetailsViewModel: WeatherDetailsViewModel) {
         Permission(
             permission = Manifest.permission.ACCESS_COARSE_LOCATION,
             permissionGranted = {
+                if (ActivityCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
+                        override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+
+                        override fun isCancellationRequested() = false
+                    })
+                        .addOnSuccessListener { location: Location? ->
+                            Log.d("LOCATIONLATLON", "lat :${location?.latitude} - lon :${location?.longitude} ")
+                        }
+                }
 
             })
         CurrentWeather()
