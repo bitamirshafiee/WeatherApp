f904294509d6476fad9075f6c50944d3f55f61df
mirshafiee <b.mirshafiee90@gmail.com>
2022-12-11 11:20:59 +0100

    complete service with providers and repositories

---
+++ app/src/main/java/com/weatherapp/repository/NetworkResult.kt
@@ -0,0 +1,6 @@
+package com.weatherapp.repository
+
+sealed class NetworkResult<T> {
+    data class Success<T>(val data: T) : NetworkResult<T>()
+    data class Failure<T>(val throwable: Throwable) : NetworkResult<T>()
+}
--- app/src/main/java/com/weatherapp/repository/service/WeatherService.kt
+++ app/src/main/java/com/weatherapp/repository/service/WeatherService.kt
@@ -1,14 +1,18 @@
 package com.weatherapp.repository.service
 
+import com.weatherapp.const.APP_ID
+import com.weatherapp.const.TEMPERATURE_UNIT
+import com.weatherapp.repository.model.WeatherResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherService {
 
-    @GET("onecall/{lat}/{lon}/{appid}")
+    @GET("onecall/{lat}/{lon}/{appid}/{units}")
     suspend fun getWeather(
         @Query("lat") latitude : Double,
         @Query("lon") longitude : Double,
-        @Query("appid") appId : String
-    )
+        @Query("appid") appId : String = APP_ID,
+        @Query("units") temperatureUnit : String = TEMPERATURE_UNIT
+    ) : WeatherResponse
 }
