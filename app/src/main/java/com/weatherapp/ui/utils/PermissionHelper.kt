14b1a09e7c4cf232b3127140c831fda5c06433ed
mirshafiee <b.mirshafiee90@gmail.com>
2022-12-11 15:43:49 +0100

    use viewmodel factory helper to inject view model

--- app/src/main/java/com/weatherapp/ui/utils/PermissionHelper.kt
+++ app/src/main/java/com/weatherapp/ui/utils/PermissionHelper.kt
@@ -10,9 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.*
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
