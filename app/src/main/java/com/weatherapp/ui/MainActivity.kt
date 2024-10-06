21525a7aacdbd59ddafaa4f94e1f03aec64a872c
mirshafiee <b.mirshafiee90@gmail.com>
2024-10-06 22:32:02 +0200

    Enable edge to edge on the app

--- app/src/main/java/com/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/weatherapp/ui/MainActivity.kt
@@ -3,9 +3,9 @@ package com.weatherapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,8 +14,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.weatherapp.ui.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppTheme {
